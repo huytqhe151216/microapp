9fed0aa72307423b6f8ac0f6a0ebc7fb87e94cd9
huy <huytqhe151216@fpt.edu.vn>
2022-09-07 15:49:55 +0700

    first commit

---
+++ retake3/moviebackend/src/main/java/com/aladin/huyreport2/repository/HuyRateRepository.java
@@ -0,0 +1,14 @@
+package com.aladin.huyreport2.repository;
+
+import com.aladin.huyreport2.domain.HuyRate;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the HuyRate entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface HuyRateRepository extends JpaRepository<HuyRate, Long> {
+}
